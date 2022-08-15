7221806d7411f01257d67fc2498c27f64c27b40c
Eduardo de Carvalho Barbosa <educarvalho.engcigil@gmail.com>
2022-08-15 16:17:05 -0300

    Versão Final

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+let comida;
 function selecionarComida (com){
+    //quando clicar em para adicionar bordavede mudar .icone para display:inline
     
     const selecionado = document.querySelector (".alimento .bordaverde");
     if (selecionado !== null){
@@ -6,8 +8,12 @@ function selecionarComida (com){
     selecionado.classList.remove("bordaverde");
     }
     const item = document.querySelector(com);
-    item.classList.toggle("bordaverde");
+    item.classList.add("bordaverde");
+    
+    comida = item.innerHTML;
+    habilitarbotao()
 }
+let bebida;
 function selecionarBebida (beb){
     
     const selecionado = document.querySelector (".bebidas .bordaverde");
@@ -17,7 +23,12 @@ function selecionarBebida (beb){
     }
     const item = document.querySelector(beb);
     item.classList.toggle("bordaverde");
+    
+
+    bebida =item.innerHTML;
+    habilitarbotao()
 }
+let sobremesa;
 function selecionarSobremesa (sob){
     
     const selecionado = document.querySelector (".sobremesas .bordaverde");
@@ -27,4 +38,40 @@ function selecionarSobremesa (sob){
     }
     const item = document.querySelector(sob);
     item.classList.toggle("bordaverde");
+   
+    
+    sobremesa = item.innerHTML;
+    habilitarbotao()
+}
+function habilitarbotao(){
+    if (comida !== undefined && bebida !== undefined && sobremesa !== undefined){
+        const botaocinza = document.querySelector('.botaocinza');
+        const botaoselecionado = document.querySelector('.botaoselecionado');
+        botaocinza.style.display="none";
+        botaoselecionado.style.display="flex";
+
+    }
+}
+function enviarwpp (){
+    let precoComida = (document.querySelector (".alimento .bordaverde .preco .numeroPreco")).innerHTML;
+
+    let precoBebida = document.querySelector (".bebidas .bordaverde .preco .numeroPreco").innerHTML;
+
+    let precoSobremesa = document.querySelector (".sobremesas .bordaverde .preco .numeroPreco ").innerHTML;
+   
+   let total =Number(precoComida)+Number(precoBebida)+Number(precoSobremesa);
+
+    let comidaConfirmada = document.querySelector (".alimento .bordaverde .nome-alimento")
+    let bebidaConfirmada = document.querySelector (".bebidas .bordaverde .nome-alimento")
+    let sobremesaConfirmada = document.querySelector (".sobremesas .bordaverde .nome-alimento")
+    let texto = `Olá, gostaria de fazer o pedido:
+                - Prato: ${comidaConfirmada.innerHTML}
+                - Bebida: ${bebidaConfirmada.innerHTML}
+                - Sobremesa: ${sobremesaConfirmada.innerHTML}
+                Total: R$`+ total.toFixed(2);
+    let textoCripto = encodeURIComponent(texto);
+
+    window.location.href ="https://wa.me/5531983683679?text="+textoCripto
+     
+    
 }
